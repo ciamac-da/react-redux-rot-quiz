1999d8a59ed14e1131bca21cac1221dca7017570
ciamac <hgzguzg>
2020-03-08 02:16:42 +0100

    Verified by Cia 1.0.01

--- src/Question.js
+++ src/Question.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
 // to import Checkbox from material-ui!!!
-import Checkbox       from '@material-ui/core/Checkbox';
+import Checkbox from '@material-ui/core/Checkbox';
 // to import Button from material-ui!!!
 // So we need it because of next Button
-import Button         from '@material-ui/core/Button';
+import Button from '@material-ui/core/Button';
 // to import Favorite from materal-ui!!!
-import Favorite       from '@material-ui/icons/Favorite';
+import Favorite from '@material-ui/icons/Favorite';
 // to import FavBorder from materail-ui!!!
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
 //Style
@@ -21,13 +21,13 @@ import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
   werden die Antworten ausgwertet und [nextQuestion] aus der [QuestionSet]
   Komponente aufgerufen.
  */
-const heart ={
-color:"black"
+const heart = {
+  color: "black"
 }
 
 
 const AnswerStyle = {
-  color:"white",
+  color: "white",
 }
 
 // Step by step!!!
@@ -38,43 +38,43 @@ const AnswerStyle = {
 // 5th : select!
 // 6th : nextQuestion
 // 7th : classes!!!
-        export default function Question({
-          question, correct, answers, chosen, select, nextQuestion ,classes 
-        }){ return (
+export default function Question({
+  question, correct, answers, chosen, select, nextQuestion, classes
+}) {
+  return (
 
-<div className="Cia" >
-    <h1>{question}</h1>
-     {answers.map(
-      (answer,index)=> (
-        <div key={index} className="answer" style={AnswerStyle}>
-          <Checkbox
-            onChange={select(index)}
-            checked={chosen[index]}
-            icon={<FavoriteBorder style={heart}/>}
-            checkedIcon={<Favorite style={AnswerStyle} />}
-          /> {answer}
-        </div> )
+    <div
+      className="Cia">
+      <h1>{question}</h1>
+      {answers.map(
+        (answer, index) => (
+          <div key={index} className="answer" style={AnswerStyle}>
+            <Checkbox
+              onChange={select(index)}
+              checked={chosen[index]}
+              icon={<FavoriteBorder style={heart} />}
+              checkedIcon={<Favorite style={AnswerStyle} />}
+            /> {answer}
+          </div>)
       )}
-      
+
       <Button variant="contained" color="disabled" onClick={
         e => {
           const wasAnsweredCorrectly = chosen.reduce(
-            (isCorrect,value,index)=> {
-              if ( ! isCorrect ) return false; // wenn eine Antwort falsch ist ist die Frage immer falsch beantwortet
-              if ( value === false && index === correct ) return false; // Wenn der Benuter nein gesag hat aber die antwor ist richtig
-              if ( value === true  && index !== correct ) return false; // Wenn der Benutzer ja gesagt hat aber die Antwort ist falsch
+            (isCorrect, value, index) => {
+              if (!isCorrect) return false; // wenn eine Antwort falsch ist ist die Frage immer falsch beantwortet
+              if (value === false && index === correct) return false; // Wenn der Benuter nein gesag hat aber die antwor ist richtig
+              if (value === true && index !== correct) return false; // Wenn der Benutzer ja gesagt hat aber die Antwort ist falsch
               return true;
             }
-          ,true);
+            , true);
           nextQuestion(wasAnsweredCorrectly ? 1 : 0);
-      }}>
-      
+        }}>
+
         Next
       </Button>
-</div>
+    </div>
 
   )
 }
-
-// export the component to app js !!!
 // I already did it as I difinded Question function!!!
