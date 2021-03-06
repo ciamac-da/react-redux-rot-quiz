900745c358fea789452bcab0c5a13207e23f8294
ciamac-da <ciamac.davoudi@gmail.com>
2021-03-06 19:34:14 +0100

    Updates

---
+++ precache-manifest.a9da0d638d03776bace7bf2fd6b12ed0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1155aa0563ad164231ecda13552ca9a5",
+    "url": "/react-redux-rot-quiz/index.html"
+  },
+  {
+    "revision": "7cef4ead7162a25485b8",
+    "url": "/react-redux-rot-quiz/static/css/main.a29be92e.chunk.css"
+  },
+  {
+    "revision": "99c2f4a8e80c304bc1fe",
+    "url": "/react-redux-rot-quiz/static/js/2.26fc68b5.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/react-redux-rot-quiz/static/js/2.26fc68b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7cef4ead7162a25485b8",
+    "url": "/react-redux-rot-quiz/static/js/main.eef860d4.chunk.js"
+  },
+  {
+    "revision": "3bc6d4f6826e51d49901",
+    "url": "/react-redux-rot-quiz/static/js/runtime-main.f3c45396.js"
+  },
+  {
+    "revision": "9a9aaec0b33198316d8493882aa7fa80",
+    "url": "/react-redux-rot-quiz/static/media/quiztime.9a9aaec0.jpg"
+  }
+]);
